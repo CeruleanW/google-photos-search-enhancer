fcdfd0f07a57e47f9c688764c76b3ba665d1e5f5
CeruleanW <yangyi6463@gmail.com>
2020-08-02 23:01:29 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Google Photos 中文搜索
+
+## Intro
+
+Google Photos is powerful for finding a photo via English. It can also support searching photos by some common words in Chinese. However, you cannot search specific text in Chinese within names or descriptions. This app will enable you to do so.
+
+## Development Tools
+
+- React.js
+- Create React App
+  - Webpack
+  - Babel
+- Material UI
+- [React Google Login](https://www.npmjs.com/package/react-google-login)
+
+## License
+
+The code is available under the [MIT license](LICENSE).
+
+## Dependencies
+
+```bash
+npm install @material-ui/core --save-dev
+npm install react-google-login --save-dev
+```
---
+++ src/components/GoogleBtn.jsx
@@ -0,0 +1,91 @@
+/* global gapi */
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { GoogleLogin, GoogleLogout, useGoogleLogout } from 'react-google-login';
+import * as credentials from './credentials.json';
+
+const oauth2 = {
+  clientID: credentials.web.client_id,
+  projectId: 'search-chinese-1595873063241',
+  authUri: 'https://accounts.google.com/o/oauth2/auth',
+  tokenUri: 'https://oauth2.googleapis.com/token',
+  scopes: [
+    'https://www.googleapis.com/auth/photoslibrary',
+    'https://www.googleapis.com/auth/photoslibrary.readonly',
+  ],
+};
+
+// const useStyles = makeStyles({
+//   gSignin2: {},
+//   icon: {
+//     maxWidth: '32px',
+//   },
+// });
+
+export default function GoogleBtn() {
+  // const classes = useStyles();
+  const [isLogined, setIsLogined] = useState(false);
+  const [accessToken, setAccessToken] = useState('');
+  const responseGoogle = (response) => {
+    console.log(response);
+  };
+
+  const login = (response) => {
+    if (response.accessToken) {
+      setIsLogined(true);
+      setAccessToken(response.accessToken);
+    }
+  };
+
+  const logout = (response) => {
+    setIsLogined(false);
+    setAccessToken('');
+  };
+
+  const handleLoginFailure = (response) => {
+    alert('Failed to log in');
+  };
+
+  const handleLogoutFailure = (response) => {
+    alert('Failed to log out');
+  };
+
+  const request = () => {
+    if (isLogined) {
+      const GoogleAuth = window.gapi.auth2.getAuthInstance();
+      const option = new gapi.auth2.SigninOptionsBuilder();
+      option.setScope('');
+
+      const googleUser = gapi.auth2.currentUser.get();
+      googleUser.grant(options).then(
+        function (success) {
+          console.log(JSON.stringify({ message: 'success', value: success }));
+        },
+        function (fail) {
+          alert(JSON.stringify({ message: 'fail', value: fail }));
+        }
+      );
+    }
+  };
+
+  return (
+    <React.Fragment>
+      {isLogined ? (
+        <GoogleLogout
+          clientId={oauth2.clientID}
+          onLogoutSuccess={logout}
+          buttonText='Logout'
+          onFailure={handleLogoutFailure}
+        ></GoogleLogout>
+      ) : (
+        <GoogleLogin
+          clientId={oauth2.clientID}
+          onSuccess={login}
+          onFailure={handleLoginFailure}
+          cookiePolicy={'single_host_origin'}
+          responseType='code,token'
+        />
+      )}
+    </React.Fragment>
+  );
+}
