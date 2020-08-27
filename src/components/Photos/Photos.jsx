01c7eb2f0e938927cb9ffbba2ded2f8a3237856f
CeruleanW <yangyi6463@gmail.com>
2020-08-27 18:28:23 -0400

    Basic Masonry Layout, done

--- src/components/Photos/Photos.jsx
+++ src/components/Photos/Photos.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { usePhotoUrl } from '../UrlsContext';
-
+import Masonry from 'react-masonry-css';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -10,6 +10,7 @@ const useStyles = makeStyles((theme) => ({
     flexWrap: 'wrap',
     overflow: 'hidden',
     backgroundColor: theme.palette.background.paper,
+    marginTop: '10px',
   },
   gridListTile: {
     maxWidth: 480,
@@ -22,35 +23,59 @@ const useStyles = makeStyles((theme) => ({
     },
   },
   offset: theme.mixins.toolbar,
+  masonryGrid: {
+    display: 'flex',
+    marginLeft: '-30px' /* gutter size offset */,
+    width: '100%',
+  },
+  masonryGridColumn: {
+    paddingLeft: '20px' /* gutter size */,
+    backgroundClip: 'padding-box',
+    /* change div to reference your elements you put in <Masonry> */
+    '& > div': {
+      marginBottom: '30px',
+    },
+  },
 }));
 
 export default function Photos() {
   const classes = useStyles();
   const photoUrls = usePhotoUrl();
+
+  // process base urls into image src urls
   let gridsBaseUrls = [];
   if (photoUrls.length > 0) {
-    gridsBaseUrls = photoUrls.map((url) => `${url.baseUrl}=w640-h480`);
+    gridsBaseUrls = photoUrls.map((url) => `${url.baseUrl}=w640-h640`);
   }
+
+  const breakpointColumnsObj = {
+    default: 4,
+    1100: 3,
+    700: 2,
+    500: 1,
+  };
 
   const handleClick = (url) => {
     window.open(url);
-  } 
+  };
 
   return (
     <div className={classes.root}>
-      <Grid container spacing={1}>
+      <Masonry
+        breakpointCols={breakpointColumnsObj}
+        className={classes.masonryGrid}
+        columnClassName={classes.masonryGridColumn}
+      >
         {gridsBaseUrls.map((imgUrl, index) => (
-          <Grid item key={index} cols={3} className={classes.gridListTile}>
-            <img
-              key={index}
-              src={imgUrl}
-              alt='Google Photos'
-              className={classes.image}
-              onClick={() => handleClick(photoUrls[index].productUrl)}
-            />
-          </Grid>
+          <img
+            key={index}
+            src={imgUrl}
+            alt='Google Photos'
+            className={classes.image}
+            onClick={() => handleClick(photoUrls[index].productUrl)}
+          />
         ))}
-      </Grid>
+      </Masonry>
     </div>
   );
 }
