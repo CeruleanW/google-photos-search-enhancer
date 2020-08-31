3140b8e6d58c2f1753c402942bf36e3634eaf08c
CeruleanW <yangyi6463@gmail.com>
2020-08-30 21:48:14 -0400

    Updates

---
+++ precache-manifest.2538c9c37325dac4b8f754afae88b637.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "975c226db175fed9723bdc5dcd9f9f3d",
+    "url": "/google-photos-search-enhancer/index.html"
+  },
+  {
+    "revision": "56ee52996b231a97e965",
+    "url": "/google-photos-search-enhancer/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "02c93b8ba8a431128b92",
+    "url": "/google-photos-search-enhancer/static/js/2.36ef3b71.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/google-photos-search-enhancer/static/js/2.36ef3b71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56ee52996b231a97e965",
+    "url": "/google-photos-search-enhancer/static/js/main.66289205.chunk.js"
+  },
+  {
+    "revision": "2bf202e0ae2a0d04d0ce",
+    "url": "/google-photos-search-enhancer/static/js/runtime-main.fc9f51fe.js"
+  },
+  {
+    "revision": "b831ad50ce27f5bbcd87a4e9beb09b20",
+    "url": "/google-photos-search-enhancer/static/media/icons8-search-500.b831ad50.png"
+  }
+]);
